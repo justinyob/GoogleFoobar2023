1f44ac1a848d14f154ca64ccbf0f29df5c5ccb7c
Justin Yob <justinyob211@gmail.com>
2023-10-09 15:09:14 -0400

    delete commented test code

--- level2_solution.py
+++ level2_solution.py
@@ -72,52 +72,6 @@ def convert(num):
     coords = [x, y]
     return coords
 
-"""
-
-def validate(coords):
-    x = coords[0]
-    y = coords[1]
-    if x > 0 and y > 0 and x < 9 and y < 9:
-        return True
-    else:
-        return False 
-
-
-
-def gen_moves(coords):
-    moves = []
-    x = coords[0]
-    y = coords[1]
-    options = [[2, 1], [2, -1], [-2, 1], [-2, -1], [1, 2], [1, -2], [-1, 2], [-1, -2]]
-    for i in options:
-        move_coords = [x, y]
-        x_move = i[0]
-        y_move = i[1]
-        move_coords[0] = move_coords[0] + x_move
-        move_coords[1] = move_coords[1] + y_move
-        if validate(move_coords):
-            moves.append(move_coords)
-            print(move_coords)
-            print("Valid Move")
-      
-def move(x, y):
-    while x != 0 or y != 0:
-        while abs(x) > abs(y):
-            print("X distance is bigger")
-            if x < 0:
-                if y <= 0:
-                    x += 2
-                    y +=1
-                print([x, y])
-            elif x
-
-        elif abs(x) == abs(y):
-            print("X and Y distance's are equal")
-            if x 
-
-        else:
-            print("Y distance is bigger")    
-"""
 def calc(x_diff, y_diff):
     tuple_dist = (x_diff, y_diff)
     for i in move_dict:
